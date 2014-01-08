8ba0213feb5a30b21dc83036c91f9238e221935e
Johannes Demel <ufcsy@student.kit.edu>
2014-01-08 00:09:23 -0800

    added hier block compile helper script

---
+++ examples/hier_block_install_helper.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2014 Communications Engineering Lab (CEL) / Karlsruhe Institute of Technology (KIT)
+#
+# This is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this software; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street,
+# Boston, MA 02110-1301, USA.
+#
+
+import glob
+import subprocess
+
+def main():
+    for file in glob.glob("*.grc"):
+        proc = ["grcc", file]
+        print proc
+        try:
+            subprocess.call(proc)
+        except:
+            pass
+
+if __name__ == "__main__":
+    main()
