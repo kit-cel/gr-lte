7f8dd62aff6128f24e7c0971cd146e1147330b0c
Kristian Maier <uafye@student.kit.edu>
2014-08-09 15:52:02 +0200

    cosmetic changes

--- examples/MIMO_TOP_FLOW.py
+++ examples/MIMO_TOP_FLOW.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python
 ##################################################
 # Gnuradio Python Flow Graph
-# Title: MIMO TOP FLOW 4 Tx ant
+# Title: MIMO TOP FLOW
 # Author: Johannes Demel, Kristian Maier
-# Description: LTE decoding up to 4 tx antennas
-# Generated: Sat Aug  9 15:51:18 2014
+# Description: LTE decoding up to 2 tx antennas
+# Generated: Sat Aug  9 15:43:57 2014
 ##################################################
 
 execfile("/home/maier/.grc_gnuradio/decode_bch_hier_gr37.py")
-execfile("/home/maier/.grc_gnuradio/lte_mimo_decode_pbch_4_txant.py")
-execfile("/home/maier/.grc_gnuradio/lte_mimo_estimator_4_txant.py")
+execfile("/home/maier/.grc_gnuradio/lte_mimo_decode_pbch.py")
+execfile("/home/maier/.grc_gnuradio/lte_mimo_estimator.py")
 execfile("/home/maier/.grc_gnuradio/lte_mimo_ofdm_rx.py")
 execfile("/home/maier/.grc_gnuradio/lte_mimo_pss_based_frey_sync.py")
 execfile("/home/maier/.grc_gnuradio/lte_mimo_pss_sync.py")
 execfile("/home/maier/.grc_gnuradio/lte_mimo_sss_sync.py")
-from PyQt4 import Qt
 from gnuradio import blocks
 from gnuradio import eng_notation
 from gnuradio import filter
@@ -23,39 +22,16 @@ from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
 from optparse import OptionParser
 import lte
-import sys
 
-from distutils.version import StrictVersion
-class MIMO_TOP_FLOW_4_TXant(gr.top_block, Qt.QWidget):
+class MIMO_TOP_FLOW(gr.top_block):
 
     def __init__(self):
-        gr.top_block.__init__(self, "MIMO TOP FLOW 4 Tx ant")
-        Qt.QWidget.__init__(self)
-        self.setWindowTitle("MIMO TOP FLOW 4 Tx ant")
-        try:
-             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except:
-             pass
-        self.top_scroll_layout = Qt.QVBoxLayout()
-        self.setLayout(self.top_scroll_layout)
-        self.top_scroll = Qt.QScrollArea()
-        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
-        self.top_scroll_layout.addWidget(self.top_scroll)
-        self.top_scroll.setWidgetResizable(True)
-        self.top_widget = Qt.QWidget()
-        self.top_scroll.setWidget(self.top_widget)
-        self.top_layout = Qt.QVBoxLayout(self.top_widget)
-        self.top_grid_layout = Qt.QGridLayout()
-        self.top_layout.addLayout(self.top_grid_layout)
-
-        self.settings = Qt.QSettings("GNU Radio", "MIMO_TOP_FLOW_4_TXant")
-        self.restoreGeometry(self.settings.value("geometry").toByteArray())
-
+        gr.top_block.__init__(self, "MIMO TOP FLOW")
 
         ##################################################
         # Variables
         ##################################################
-        self.fftl = fftl = 1024
+        self.fftl = fftl = 2048
         self.samp_rate = samp_rate = fftl*15e3
         self.rxant = rxant = 2
         self.resampler = resampler = 400
@@ -96,13 +72,13 @@ class MIMO_TOP_FLOW_4_TXant(gr.top_block, Qt.QWidget):
             N_rb_dl=N_rb_dl,
             rxant=rxant,
         )
-        self.lte_mimo_estimator_4_txant_0 = lte_mimo_estimator_4_txant(
+        self.lte_mimo_estimator_0 = lte_mimo_estimator(
             estimator_key=frame_key,
             N_rb_dl=N_rb_dl,
+            initial_id=2,
             rxant=rxant,
-            initial_id=333,
         )
-        self.lte_mimo_decode_pbch_4_txant_0 = lte_mimo_decode_pbch_4_txant(
+        self.lte_mimo_decode_pbch_0 = lte_mimo_decode_pbch(
             N_rb_dl=N_rb_dl,
             rxant=rxant,
         )
@@ -115,38 +91,32 @@ class MIMO_TOP_FLOW_4_TXant(gr.top_block, Qt.QWidget):
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.lte_mimo_sss_sync_0, 0), (self.lte_mimo_estimator_4_txant_0, 0))
-        self.connect((self.lte_mimo_sss_sync_0, 0), (self.lte_mimo_decode_pbch_4_txant_0, 0))
-        self.connect((self.lte_mimo_estimator_4_txant_0, 0), (self.lte_mimo_decode_pbch_4_txant_0, 1))
-        self.connect((self.lte_mimo_estimator_4_txant_0, 1), (self.lte_mimo_decode_pbch_4_txant_0, 2))
-        self.connect((self.blocks_file_source_0, 0), (self.blocks_throttle_0, 0))
-        self.connect((self.lte_mimo_decode_pbch_4_txant_0, 0), (self.bch_decode_bch_hier_gr37_0, 0))
-        self.connect((self.bch_decode_bch_hier_gr37_0, 0), (self.MIB, 0))
-        self.connect((self.bch_decode_bch_hier_gr37_0, 1), (self.MIB, 1))
-        self.connect((self.lte_mimo_estimator_4_txant_0, 3), (self.lte_mimo_decode_pbch_4_txant_0, 4))
-        self.connect((self.lte_mimo_estimator_4_txant_0, 2), (self.lte_mimo_decode_pbch_4_txant_0, 3))
-        self.connect((self.rational_resampler_xxx_0_0, 0), (self.lte_mimo_pss_sync_0, 1))
         self.connect((self.rational_resampler_xxx_0_1, 0), (self.lte_mimo_pss_sync_0, 0))
         self.connect((self.blocks_vector_to_streams_0, 0), (self.rational_resampler_xxx_0_1, 0))
-        self.connect((self.blocks_vector_to_streams_0, 1), (self.rational_resampler_xxx_0_0, 0))
-        self.connect((self.lte_mimo_pss_sync_0, 1), (self.lte_mimo_pss_based_frey_sync_0, 1))
+        self.connect((self.blocks_file_source_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.blocks_vector_to_streams_0, 0))
         self.connect((self.lte_mimo_ofdm_rx_0, 0), (self.lte_mimo_sss_sync_0, 0))
         self.connect((self.lte_mimo_pss_based_frey_sync_0, 1), (self.lte_mimo_ofdm_rx_0, 1))
         self.connect((self.lte_mimo_pss_based_frey_sync_0, 0), (self.lte_mimo_ofdm_rx_0, 0))
         self.connect((self.lte_mimo_pss_sync_0, 0), (self.lte_mimo_pss_based_frey_sync_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.blocks_vector_to_streams_0, 0))
+        self.connect((self.lte_mimo_pss_sync_0, 1), (self.lte_mimo_pss_based_frey_sync_0, 1))
+        self.connect((self.lte_mimo_decode_pbch_0, 0), (self.bch_decode_bch_hier_gr37_0, 0))
+        self.connect((self.lte_mimo_estimator_0, 0), (self.lte_mimo_decode_pbch_0, 1))
+        self.connect((self.lte_mimo_estimator_0, 1), (self.lte_mimo_decode_pbch_0, 2))
+        self.connect((self.bch_decode_bch_hier_gr37_0, 0), (self.MIB, 0))
+        self.connect((self.bch_decode_bch_hier_gr37_0, 1), (self.MIB, 1))
+        self.connect((self.lte_mimo_sss_sync_0, 0), (self.lte_mimo_decode_pbch_0, 0))
+        self.connect((self.lte_mimo_sss_sync_0, 0), (self.lte_mimo_estimator_0, 0))
+        self.connect((self.blocks_vector_to_streams_0, 1), (self.rational_resampler_xxx_0_0, 0))
+        self.connect((self.rational_resampler_xxx_0_0, 0), (self.lte_mimo_pss_sync_0, 1))
 
         ##################################################
         # Asynch Message Connections
         ##################################################
-        self.msg_connect(self.lte_mimo_sss_sync_0, "cell_id", self.lte_mimo_estimator_4_txant_0, "cell_id")
-        self.msg_connect(self.lte_mimo_sss_sync_0, "cell_id", self.lte_mimo_decode_pbch_4_txant_0, "cell_id")
         self.msg_connect(self.lte_mimo_pss_sync_0, "N_id_2", self.lte_mimo_sss_sync_0, "N_id_2")
+        self.msg_connect(self.lte_mimo_sss_sync_0, "cell_id", self.lte_mimo_decode_pbch_0, "cell_id")
+        self.msg_connect(self.lte_mimo_sss_sync_0, "cell_id", self.lte_mimo_estimator_0, "cell_id")
 
-    def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "MIMO_TOP_FLOW_4_TXant")
-        self.settings.setValue("geometry", self.saveGeometry())
-        event.accept()
 
     def get_fftl(self):
         return self.fftl
@@ -154,9 +124,9 @@ class MIMO_TOP_FLOW_4_TXant(gr.top_block, Qt.QWidget):
     def set_fftl(self, fftl):
         self.fftl = fftl
         self.set_samp_rate(self.fftl*15e3)
-        self.lte_mimo_pss_sync_0.set_fftlen(self.fftl)
-        self.lte_mimo_pss_based_frey_sync_0.set_fftlen(self.fftl)
         self.lte_mimo_ofdm_rx_0.set_fftlen(self.fftl)
+        self.lte_mimo_pss_based_frey_sync_0.set_fftlen(self.fftl)
+        self.lte_mimo_pss_sync_0.set_fftlen(self.fftl)
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -170,12 +140,12 @@ class MIMO_TOP_FLOW_4_TXant(gr.top_block, Qt.QWidget):
 
     def set_rxant(self, rxant):
         self.rxant = rxant
-        self.lte_mimo_pss_sync_0.set_rxant(self.rxant)
-        self.lte_mimo_pss_based_frey_sync_0.set_rxant(self.rxant)
-        self.lte_mimo_decode_pbch_4_txant_0.set_rxant(self.rxant)
-        self.lte_mimo_estimator_4_txant_0.set_rxant(self.rxant)
+        self.lte_mimo_estimator_0.set_rxant(self.rxant)
+        self.lte_mimo_decode_pbch_0.set_rxant(self.rxant)
         self.lte_mimo_sss_sync_0.set_rxant(self.rxant)
         self.lte_mimo_ofdm_rx_0.set_rxant(self.rxant)
+        self.lte_mimo_pss_based_frey_sync_0.set_rxant(self.rxant)
+        self.lte_mimo_pss_sync_0.set_rxant(self.rxant)
 
     def get_resampler(self):
         return self.resampler
@@ -188,7 +158,7 @@ class MIMO_TOP_FLOW_4_TXant(gr.top_block, Qt.QWidget):
 
     def set_frame_key(self, frame_key):
         self.frame_key = frame_key
-        self.lte_mimo_estimator_4_txant_0.set_estimator_key(self.frame_key)
+        self.lte_mimo_estimator_0.set_estimator_key(self.frame_key)
         self.lte_mimo_ofdm_rx_0.set_ofdm_key(self.frame_key)
 
     def get_N_rb_dl(self):
@@ -196,31 +166,16 @@ class MIMO_TOP_FLOW_4_TXant(gr.top_block, Qt.QWidget):
 
     def set_N_rb_dl(self, N_rb_dl):
         self.N_rb_dl = N_rb_dl
-        self.lte_mimo_decode_pbch_4_txant_0.set_N_rb_dl(self.N_rb_dl)
-        self.lte_mimo_estimator_4_txant_0.set_N_rb_dl(self.N_rb_dl)
+        self.lte_mimo_estimator_0.set_N_rb_dl(self.N_rb_dl)
+        self.lte_mimo_decode_pbch_0.set_N_rb_dl(self.N_rb_dl)
         self.lte_mimo_sss_sync_0.set_N_rb_dl(self.N_rb_dl)
         self.lte_mimo_ofdm_rx_0.set_N_rb_dl(self.N_rb_dl)
 
 if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print "Warning: failed to XInitThreads()"
     parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
     (options, args) = parser.parse_args()
-    if(StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0")):
-        Qt.QApplication.setGraphicsSystem(gr.prefs().get_string('qtgui','style','raster'))
-    qapp = Qt.QApplication(sys.argv)
-    tb = MIMO_TOP_FLOW_4_TXant()
+    tb = MIMO_TOP_FLOW()
     tb.start()
-    tb.show()
-    def quitting():
-        tb.stop()
-        tb.wait()
-    qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
-    qapp.exec_()
-    tb = None #to clean up Qt widgets
+    raw_input('Press Enter to quit: ')
+    tb.stop()
+    tb.wait()
